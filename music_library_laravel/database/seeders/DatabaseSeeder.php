3fc5c8f0516f16d877ababf42bd9d6c36a931487
Aleksandra Pantelic <saskapantelic6@gmail.com>
2023-02-07 15:06:25 +0100

    fourth commit - Napravljen seeder

--- music_library_laravel/database/seeders/DatabaseSeeder.php
+++ music_library_laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Artist;
+use App\Models\Genre;
+use App\Models\Song;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,14 +17,94 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    /*
+    
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Song::truncate();
+        Artist::truncate();
+        Genre::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }*/
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $genre1 = Genre::factory()->create();
+        $genre2 = Genre::factory()->create();
+        $genre3 = Genre::factory()->create();
+
+        $artist1 = Artist::factory()->create();
+        $artist2 = Artist::factory()->create();
+        $artist3 = Artist::factory()->create();
+
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre1->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist2->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist2->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist3->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user1->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist3->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre1->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist1->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist2->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist2->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre2->id,
+            'artist_id' => $artist3->id
+        ]);
+        Song::factory(2)->create([
+            'user_id' => $user2->id,
+            'genre_id' => $genre3->id,
+            'artist_id' => $artist3->id
+        ]);
+    }
 }
